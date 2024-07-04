ef4337b6dec5bbaec986dfd3b28ba36b86fa7aba
NandanXDm <nanven9@gmail.com>
2024-07-04 18:03:45 +0530

    Youtube summarization api added

--- constants.js
+++ constants.js
@@ -1,1 +1,53 @@
-export const geminiUrl=''
+export const geminiUrl = ''
+
+const ResumeFormat = {
+    name: '',
+    role: '',
+
+    contact: {
+        location: '',
+        gmail: '',
+        linkedIn: '',
+        github: '',
+        phoneNumber: '',
+    },
+
+    experience: [
+        {
+            company: '',
+            location: '',
+            role: '',
+            startYear: '',
+            endYear: '',
+            responsibilities: [
+                {
+                    description: '',
+                    skills: []
+                }
+            ]
+        }
+    ],
+
+    skills: [
+
+    ],
+
+    eduction: [
+        {
+            organization: '',
+            startYear: '',
+            endYear: '',
+            specialization: '',
+            degree: '',
+            grade: ''
+        }
+    ],
+
+    projects: [
+        {
+            name: '',
+            skills: [],
+            description: ''
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const pdf = require('pdf-parse');
 
+const { YoutubeTranscript } = require('youtube-transcript');
+
 require('dotenv').config();
 const PORT = process.env.PORT;
 
@@ -20,34 +22,101 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash-latest",
+    generationConfig:{"response_mime_type": "application/json"}
 });
 
 const generationConfig = {
     temperature: 1,
     topP: 0.95,
     topK: 64,
-    maxOutputTokens: 8192,
     responseMimeType: "text/json",
 };
 
 async function getResultsFromGemini(resumeTextDump) {
 
     const prompt=`
+        
         INPUT: 'Text from a resume',
         INPUT TEXT:\"${resumeTextDump}\",
-        OUTPUT: 'Get resume out of the text input,send Response exactly like OUTPUT STRUCTURE',
+        OUTPUT: 'Get resume out of the text input,send Response exactly like OUTPUT STRUCTURE, Return isResume=false as a response if text is not from a resume',
         OUTPUT STRUCTURE: {
-            resume:"",
+            resume:{
+                name: '',
+                role: '',
+            
+                contact: {
+                    location: '',
+                    gmail: '',
+                    linkedIn: '',
+                    github: '',
+                    phoneNumber: '',
+                },
+            
+                experience: [
+                    {
+                        company: '',
+                        location: '',
+                        role: '',
+                        startYear: '',
+                        endYear: '',
+                        responsibilities: [
+                            {
+                                description: '',
+                                skills: []
+                            }
+                        ]
+                    }
+                ],
+            
+                skills: [
+            
+                ],
+            
+                education: [
+                    {
+                        organization: '',
+                        startYear: '',
+                        endYear: '',
+                        specialization: '',
+                        degree: '',
+                        grade: ''
+                    }
+                ],
+            
+                projects: [
+                    {
+                        name: '',
+                        skills: [],
+                        description: ''
+                    }
+                ],
+
+                achievements:[
+
+                ]
+            },
+            isResume:boolean
+            suggestionsforImprovement:[],
             keywords:[]
         }, 
         where resume in  OUTPUT STRUCTURE will be resume that you create fcrom text and
         keywords will be IT and Technical skill keywords only,
         Example Output: {
-            resume:'Nandan T S\n Frontend developer\n\nskills: python,javascript,angular....'
-            keywords:['python','javascript'...]
-        },ONLY HARD TECHNICAL SKILLS`
+            resume:{
+                name:Nandan T S,
+                role:'Frontend Developer',
+                ...
+            }
+            
+            suggestionsforImprovement:['..','....'],
+            keywords:['python','javascript'...],
+        },ONLY HARD TECHNICAL SKILLS,DONT SKIP newline, spaces in text, Give suggestions for improvements in resume and put them in list,
+      `
+
+
 
     const result = await model.generateContent(prompt);
+    
     const response = await result.response;
     const text = response.text();
 
@@ -55,12 +124,28 @@ async function getResultsFromGemini(resumeTextDump) {
 
     console.log(text);
 
-    return text.match(pattern).at(0);
+    return text;
+}
+
+
+async function getYoutubeVideoSummary(videoTranscript){
+    const prompt=`You are a youtube video summarizer ai, summarize the video transcript in less than 300 words, 
+        INPUT:${videoTranscript},
+        OUTPUT_STRUCTURE: {videoSummary:''}`
+    const result = await model.generateContent(prompt);
+
+    console.log(result);
+    
+    const response = await result.response;
+    const text = response.text();
+    return text;
 }
 
 
 app.get('/', function (req, res) {
+    
     res.send('Service Running');
+    
 })
 
 
@@ -80,6 +165,22 @@ app.post('/upload', function (req, res) {
     }).then((geminiResponse)=>{
         res.send(geminiResponse);
     });
+
+});
+
+
+app.get('/summary',async function (req, res) {
+    const transcriptResponse=await YoutubeTranscript.fetchTranscript(req.query.videoId);
+
+    let transcriptResponseText='';
+
+    for(let {text} of transcriptResponse){
+        transcriptResponseText+=text;
+    }
+
+    const response=await getYoutubeVideoSummary(transcriptResponseText);
+
+    res.send(response);
 
 });
 
